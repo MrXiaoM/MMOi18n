96fdf6174e93d039ee8727ad0aba295f945e77ad
MrXiaoM <coolxiaom95@gmail.com>
2026-09-07 08:06:44 +0800

    upgrade dependencies

--- MMOItems/6.10.1/build.gradle.kts
+++ MMOItems/6.10.1/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     java
-    id ("com.gradleup.shadow") version "8.3.0"
+    id ("com.gradleup.shadow") version "9.3.0"
 }
 
 val nbtapi: String by ext
 val targetJavaVersion = 17
-version = "${version}_mi-6.10.1"
+val miVersion = "6.10.1"
 
 repositories {
     maven("https://nexus.phoenixdevt.fr/repository/maven-public/") {
@@ -21,7 +21,7 @@ dependencies {
 
     compileOnly("net.milkbowl.vault:VaultAPI:1.7")
     compileOnly("io.lumine:MythicLib-dist:1.7.1-SNAPSHOT")
-    compileOnly("net.Indyuce:MMOItems-API:6.10.1-SNAPSHOT")
+    compileOnly("net.Indyuce:MMOItems-API:${miVersion}-SNAPSHOT")
 
     compileOnly("commons-lang:commons-lang:2.6")
     compileOnly("org.jetbrains:annotations:24.0.0")
@@ -39,11 +39,12 @@ java {
 tasks {
     shadowJar {
         archiveBaseName.set("MMOi18n")
+        configurations.add(project.configurations.runtimeClasspath.get())
     }
-    val copyTask = create<Copy>("copyBuildArtifact") {
+    val copyTask = register<Copy>("copyBuildArtifact") {
         dependsOn(shadowJar)
         from(shadowJar.get().outputs)
-        rename { "${shadowJar.get().archiveBaseName.get()}-$version.jar" }
+        rename { "${shadowJar.get().archiveBaseName.get()}-${version}_mi-${miVersion}.jar" }
         into(rootProject.file("out"))
     }
     build {
